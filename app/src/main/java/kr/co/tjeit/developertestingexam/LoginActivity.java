c9642fe7a24f61fa1c6809012478b91830dcb034
pyj2001sus <pyj2001sus@hanmail.net>
2017-09-29 10:39:28 +0900

    로그인버튼 오류 수정

--- app/src/main/java/kr/co/tjeit/developertestingexam/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/LoginActivity.java
@@ -1,5 +1,6 @@
 package kr.co.tjeit.developertestingexam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import kr.co.tjeit.developertestingexam.data.User;
 import kr.co.tjeit.developertestingexam.util.ServerUtil;
 
 public class LoginActivity extends BaseActivity {
@@ -32,18 +34,28 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
 
-                ServerUtil.sign_in(mContext, idEdt.getText().toString(), pwEdt.getText().toString(), new ServerUtil.JsonResponseHandler() {
-                    @Override
-                    public void onResponse(JSONObject json) {
-                        try {
-                            if (json.getBoolean("result")){
-                                Toast.makeText(mContext, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                ServerUtil.sign_in(mContext, idEdt.getText().toString(),
+                        pwEdt.getText().toString(), new ServerUtil.JsonResponseHandler() {
+                            @Override
+                            public void onResponse(JSONObject json) {
+
+                                try {
+                                    if (json.getBoolean("result")) {
+
+                                        User loginUser = User.getUserFromJson(json.getJSONObject("user"));
+
+                                        Toast.makeText(mContext, "로그인한사람 :" + loginUser.getName(), Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        Toast.makeText(mContext, "로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                        });
             }
         });
     }
