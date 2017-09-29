f238deaa58cda6cdf8cd4d9ebd9060f36c88ce16
Iksang <thsdlrtkd@gmail.com>
2017-09-29 10:34:01 +0900

    로그인 액티비티 구현 완료

--- app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
@@ -1,14 +1,28 @@
 package kr.co.tjeit.developertestingexam;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import kr.co.tjeit.developertestingexam.util.ServerUtil;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.Button loginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bindViews();
         setupEvents();
         setupValues();
@@ -16,6 +30,30 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ServerUtil.sign_in(mContext, idEdt.getText().toString(), pwEdt.getText().toString(), new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        try {
+                            if(json.getBoolean("result")){
+                                JSONObject user = json.getJSONObject("user");
+                                String name = user.getString("name");
+                                Toast.makeText(mContext, name+"님! 접속을 환영합니다", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                String message = json.getString("message");
+                                Toast.makeText(mContext, "로그인 실패!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
@@ -26,6 +64,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
 
     }
 }
