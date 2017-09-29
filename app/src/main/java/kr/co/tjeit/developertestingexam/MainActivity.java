a23b1e9217a6b65b0c585e08c13ac60c4c9a76f3
kdy101289 <kdy101289@gmail.com>
2017-09-29 10:18:43 +0900

    login xml 구현. (이벤트 처리 전)

--- app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
@@ -1,13 +1,43 @@
 package kr.co.tjeit.developertestingexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private EditText idEdt;
+    private EditText pwEdt;
+    private Button loginBtn;
+    private TextView signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.signupBtn = (TextView) findViewById(R.id.signupBtn);
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
     }
 }
