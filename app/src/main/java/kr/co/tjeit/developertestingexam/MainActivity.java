79c0c078a922a66758baa310bcc4fa5c6c3e0ab8
unripeblue <syp06@naver.com>
2017-09-29 10:02:15 +0900

    MainActivity Login화면 XML 그리기 작업까지 진행

--- app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
@@ -1,13 +1,38 @@
 package kr.co.tjeit.developertestingexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
     }
 }
