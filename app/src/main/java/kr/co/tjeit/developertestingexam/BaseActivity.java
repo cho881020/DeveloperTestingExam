434528b75d3a75acb0158f1fa08df520b5c6d1f7
HanSangYeol <samh2006@naver.com>
2017-09-29 09:54:10 +0900

    Branch생성

---
+++ app/src/main/java/kr/co/tjeit/developertestingexam/BaseActivity.java
@@ -0,0 +1,20 @@
+package kr.co.tjeit.developertestingexam;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by the on 2017-09-29.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindView();
+
+
+
+}
