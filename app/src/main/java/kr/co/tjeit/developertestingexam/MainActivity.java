568480007a0660a786dc04d0d5b22d75ff887372
henylee <asqaws1@gmail.com>
2017-09-29 10:10:43 +0900

    로그인화면 구현 일단 토스트로 로그인성고만 띄우기

--- app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/MainActivity.java
@@ -1,13 +1,48 @@
 package kr.co.tjeit.developertestingexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.Button loginbtn;
+    private android.widget.Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.signupBtn = (Button) findViewById(R.id.signupBtn);
+        this.loginbtn = (Button) findViewById(R.id.loginbtn);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
     }
 }
---
+++ app/src/main/java/kr/co/tjeit/developertestingexam/ServerUtil.java
@@ -0,0 +1,8 @@
+package kr.co.tjeit.developertestingexam;
+
+/**
+ * Created by joeun on 2017-09-29.
+ */
+
+public class ServerUtil {
+}
