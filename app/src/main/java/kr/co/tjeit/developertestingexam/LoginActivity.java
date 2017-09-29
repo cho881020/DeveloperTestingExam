ce8b5a669113f3b65412b082da31fde89eaa6fd2
HanSangYeol <samh2006@naver.com>
2017-09-29 10:04:43 +0900

    BaseActivity 생성/LoginAcvtivity xml완성

--- app/src/main/java/kr/co/tjeit/developertestingexam/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/developertestingexam/LoginActivity.java
@@ -1,9 +1,16 @@
 package kr.co.tjeit.developertestingexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends BaseActivity {
+
+    private android.widget.EditText idEdt;
+    private android.widget.EditText pwEdt;
+    private android.widget.Button signUpBtn;
+    private android.widget.Button idpwFindBtn;
+    private android.widget.Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,11 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindView() {
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+        this.idpwFindBtn = (Button) findViewById(R.id.idpwFindBtn);
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.pwEdt = (EditText) findViewById(R.id.pwEdt);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
 
     }
 }
